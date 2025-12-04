4c57c93ff178c4517b83a341d8662d20ea14bd8d
Mohammed Asif <apasif243@gmail.com>
2025-12-04 17:42:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-528433304-25094",
+  "appId": "1:870923081536:web:92aa54af1f9a1db7f30ca5",
+  "apiKey": "AIzaSyD72_jZPt-Fhw_q7vR0DA4IhfaFn_bO_NI",
+  "authDomain": "studio-528433304-25094.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "870923081536"
+};
